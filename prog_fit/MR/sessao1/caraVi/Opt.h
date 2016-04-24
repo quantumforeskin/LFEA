58f9648ea762d55288dbc7997923a9f36cc399bb
Pedro <pedromanpereira@gmail.com>
2016-04-24 04:08:05 +0100

    lol

---
+++ prog_fit/MR/sessao1/caraVi/Opt.h
@@ -0,0 +1,31 @@
+#include "TGraphErrors.h"
+#include "TH1F.h"
+#include "TNamed.h"
+#include <vector>
+#include <cstdlib>
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Opt
+{
+  public:
+    Opt(string, string);
+    vector<string> Escolher();
+    TH1F* Histograma();
+    void Ajuste(TGraphErrors* gr);
+    TGraphErrors* Grafico();
+    vector<double> Return_dims();
+
+  private:
+    string ficheiro;
+    string dados;
+    string opcao;
+    string func;
+    string titulo;
+    vector<double> dim;
+    vector<string> mainsender;
+    string numbin;
+};
