db6d43cb45245c0f36d0f3cbc59977454114fb3a
Miguel Gonçalves <miguel_goncalves_sg@hotmail.com>
2016-05-01 23:47:32 +0100

    a

---
+++ MR/prog_resultados/file_reader.C
@@ -0,0 +1,198 @@
+#include "file_reader.h"
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+
+int fr::getN(string file1){
+
+
+  ifstream file;
+  file.open (file1.c_str());
+
+  static int N=0;
+
+  
+  char cur = '\0';
+  char last = '\0';
+
+  while (file.get(cur)) {
+      if (cur == '\n' ||
+          (cur == '\f' && last == '\r'))
+         N++;
+  }
+
+  return N;
+
+}
+
+
+
+double* fr::getH(string file1, double I){
+
+  double eI=0.0000001;//erro corrente
+  double eV = 0.000001; //erro tensao
+  double eh = 0.1;//erro campo
+
+  ifstream file;
+  file.open (file1.c_str());
+
+  static int N=0;
+
+  
+  char cur = '\0';
+  char last = '\0';
+
+  while (file.get(cur)) {
+      if (cur == '\n' ||
+          (cur == '\f' && last == '\r'))
+         N++;
+  }
+
+  //N--;
+  
+  cout << N << endl;
+
+  double *H = new double[N];
+  double *R = new double[N];
+  double *eR = new double[N];
+
+
+  file.close();
+  file.open (file1.c_str());
+
+  int i=0;
+  int test=0;
+  while(!file.eof() && test==0)
+    {
+
+      file >> H[i] >> R[i]; // extracts 2 floating point values seperated by whitespace
+      R[i]=R[i]/I; //PASSAR TENSOES PARA RESISTENCIAS
+      eR[i] = eV/I + R[i]/I*eI; //erro resistencia
+      i++; 
+
+
+      if(i>=N)
+	test=1;
+
+    }
+
+  delete [] R;
+  delete [] eR;
+
+  return H;
+
+}
+
+double* fr::getR(string file1, double I){
+
+  double eI=0.0000001;//erro corrente
+  double eV = 0.000001; //erro tensao
+  double eh = 0.1;//erro campo
+
+  ifstream file;
+  file.open (file1.c_str());
+
+  static int N=0;
+
+  
+  char cur = '\0';
+  char last = '\0';
+
+  while (file.get(cur)) {
+      if (cur == '\n' ||
+          (cur == '\f' && last == '\r'))
+         N++;
+  }
+
+  //N--;
+  
+  cout << N << endl;
+
+  double *H = new double[N];
+  double *R = new double[N];
+  double *eR = new double[N];
+
+
+  file.close();
+  file.open (file1.c_str());
+
+  int i=0;
+  int test=0;
+  while(!file.eof() && test==0)
+    {
+
+      file >> H[i] >> R[i]; // extracts 2 floating point values seperated by whitespace
+      R[i]=R[i]/I; //PASSAR TENSOES PARA RESISTENCIAS
+      eR[i] = eV/I + R[i]/I*eI; //erro resistencia
+      i++; 
+
+
+      if(i>=N)
+	test=1;
+
+    }
+
+  delete [] H;
+  delete [] eR;
+
+  return R;
+
+}
+
+double* fr::get_eR(string file1, double I){
+
+  double eI=0.0000001;//erro corrente
+  double eV = 0.000001; //erro tensao
+  double eh = 0.1;//erro campo
+
+  ifstream file;
+  file.open (file1.c_str());
+
+  static int N=0;
+
+  
+  char cur = '\0';
+  char last = '\0';
+
+  while (file.get(cur)) {
+      if (cur == '\n' ||
+          (cur == '\f' && last == '\r'))
+         N++;
+  }
+
+  //N--;
+  
+  cout << N << endl;
+
+  double *H = new double[N];
+  double *R = new double[N];
+  double *eR = new double[N];
+
+
+  file.close();
+  file.open (file1.c_str());
+
+  int i=0;
+  int test=0;
+  while(!file.eof() && test==0)
+    {
+
+      file >> H[i] >> R[i]; // extracts 2 floating point values seperated by whitespace
+      R[i]=R[i]/I; //PASSAR TENSOES PARA RESISTENCIAS
+      eR[i] = eV/I + R[i]/I*eI; //erro resistencia
+      i++; 
+
+
+      if(i>=N)
+	test=1;
+
+    }
+
+  delete [] R;
+  delete [] H;
+
+  return eR;
+
+}
---
+++ MR/prog_resultados/file_reader.h
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+
+class fr{
+
+
+ public:
+    int getN(string);
+    double* getH(string, double);
+    double* getR(string, double);
+    double* get_eR(string, double);	
+
+
+};
